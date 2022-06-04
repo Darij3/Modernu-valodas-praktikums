60d4f77f0bc7729d1158b0364b8a12f2a386124a
Darij3 <34334294+Darij3@users.noreply.github.com>
2022-06-04 11:28:54 +0300

    Add files via upload

---
+++ MVlab1/src/Lab1.java
@@ -0,0 +1,124 @@
+
+// class employee	  
+
+interface FirmServices {
+
+	double getMaxSalary();
+}
+
+class Human {
+	protected String name;
+	protected String surname;
+	protected double salary;
+
+	public Human(String name, String surname) {
+		this.name = name;
+		this.surname = surname;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	@Override
+	public String toString() {
+		return ("Vards: " + this.getName() + " surname: " + this.getSurname());
+	}
+}
+
+//employee extended class
+class Employee extends Human {
+
+	protected double salary;
+
+	public Employee(String name, String surname, double salary) {
+
+		super(name, surname);
+		this.salary = salary;
+	}
+
+	public Employee(Employee e) {
+		this(e.getName(), e.getSurname(), e.getSalary());
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	@Override
+	public String toString() {
+		return (super.toString() + ", salary: " + this.getSalary());
+	}
+
+}
+
+//class Firm
+class Firm implements FirmServices {
+	protected String name;
+	protected Employee[] empl;
+	protected int emplAmount = 0;
+
+	public Firm(String name, int maxCount) {
+		this.name = name;
+		empl = new Employee[maxCount];
+		// this.Employee=Employee;
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getemplAmount() {
+		return emplAmount;
+	}
+
+	@Override
+	public String toString() {
+		String out = "Firma: " + this.getName() + " darbnieku daudzums: " + this.getemplAmount() + "\n";
+
+		for (int i = 0; i < emplAmount; i++)
+			out += (empl[i] + "\n");
+
+		return (out);
+	}
+
+	void add(Employee e) {
+
+		try {
+			empl[emplAmount] = new Employee(e);
+			emplAmount++;
+			// System.out.println(Employee[3]);
+		} catch (ArrayIndexOutOfBoundsException exc) {
+			System.out.println("Masivs ir pilns, nevar pievienot lietotaju.");
+		}
+	}
+
+	// maksimala alga.
+	public double getMaxSalary() {
+		double finalMaxSalary = 0;
+		for (int i = 0; i < emplAmount; i++)
+			if (empl[i].getSalary() > finalMaxSalary)
+				finalMaxSalary = empl[i].getSalary();
+
+		return (finalMaxSalary);
+	}
+}
+
+public class Lab1 {
+	public static void main(String args[]) {
+		Employee John = new Employee("John", "Smith", 1000);
+		System.out.println(John);
+		Firm VEF = new Firm("Vef", 2);
+		System.out.println(VEF);
+		VEF.add(new Employee("Janis", "Jansons", 1700.0));
+		VEF.add(new Employee("Uldis", "Petrovs", 1600.0));
+		VEF.add(new Employee("Janis", " Strods", 1800.0));
+		System.out.println(VEF);
+		System.out.println(VEF.getMaxSalary());
+	}
+}
